a6311cba3f11e6cea96774437fb47209b4f8e8c8
WesleyG <wes.gillis@gmail.com>
2014-08-16 07:58:51 -0700

    initial http://localhost:3000/helloworld is working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. WGG 8/16/2014 original
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+*/
 
+/* 
+WGG 8/16/2014 hello world home page
+  this handles the URL routing
+*/
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'Hello, World!'})
+});
 module.exports = router;
